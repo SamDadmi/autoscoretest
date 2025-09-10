f1e5fd0420a122f7610ff0f0053ada63906c7c98
Sameeksha Dadmi <163865905+SamDadmi@users.noreply.github.com>
2025-09-10 18:40:55 +0530

    Update sum_two.py

---
+++ sum_two.py
@@ -0,0 +1,11 @@
+def sum_two(a, b):
+    """Return sum of two numbers.
+
+    Args:
+        a (int | float): first number
+        b (int | float): second number
+
+    Returns:
+        int | float: sum of a and b
+    """
+    return a + b
